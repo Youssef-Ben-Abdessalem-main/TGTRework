f518760bf9b4027bb66c581fe73f7fd76516a026
Youssef-Ben-Abdessalem-main <benabdessalemyoussef7@gmail.com>
2025-12-01 09:14:00 +0100

    update

--- src/widget/Home.vue
+++ src/widget/Home.vue
@@ -1,16 +1,19 @@
 <script setup>
-import FeaturedDestinations from './Home/FeaturedDestinations.vue';
-import HeroSection from './Home/HeroSection.vue';
-import PopularTours from './Home/PopularTours.vue';
-import SearchBooking from './Home/SearchBooking.vue';
-import WhyChooseUs from './Home/WhyChooseUs.vue';
-
+import FeaturedDestinations from "./Home/FeaturedDestinations.vue";
+import HeroSection from "./Home/HeroSection.vue";
+import PopularTours from "./Home/PopularTours.vue";
+import SearchBooking from "./Home/SearchBooking.vue";
+import WhyChooseUs from "./Home/WhyChooseUs.vue";
 </script>
 
 <template>
-    <HeroSection />
+  <HeroSection />
+  <div class="max-w-7xl mx-auto">
     <SearchBooking />
     <FeaturedDestinations />
-    <PopularTours />
+  </div>
+  <PopularTours />
+  <div class="max-w-7xl mx-auto">
     <WhyChooseUs />
+  </div>
 </template>
